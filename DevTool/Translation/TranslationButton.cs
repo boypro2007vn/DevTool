e411295d042fd88de70903569bcd77493c21e260
boypro2007vn <namintelvn@gmail.com>
2018-06-10 15:43:41 +0700

    remove variable and function not use

--- DevTool/Translation/TranslationButton.cs
+++ DevTool/Translation/TranslationButton.cs
@@ -13,18 +13,10 @@ namespace DevTool.Translation
 {
     public partial class TranslationButton : Form
     {
-        #region Const Variable
-
-        private const string REGEX =
-            @"<span id=result_box (?:\s*[a-z_\-]+(?:=(?:""[^""]*""|'[^']*'|[^>]+))?)*>([^<]+)</span>";
-
-        private const string GOOGLE_URL = "https://translate.google.com/?hl=en&ie=UTF8&";
-
-        #endregion
         #region Private Variable
 
         private IKeyboardMouseEvents _jKeyboardMouseEvents;
-        private WebClient _webClient;
+        private Translator _translator;
         private TranslationResult _frmResult;
         private string _inputLang;
         private string _outputLang;
@@ -117,42 +109,17 @@ namespace DevTool.Translation
             this.TopMost = true;
         }
 
-        private string Translation(string url)
-        {
-            string result = String.Empty;
-            try
-            {
-                
-                Cursor.Current = Cursors.WaitCursor;
-
-                _webClient = new WebClient();
-                _webClient.Headers.Add(HttpRequestHeader.UserAgent, "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36");
-                _webClient.Headers.Add(HttpRequestHeader.AcceptCharset, "UTF-8");
-                _webClient.Encoding = Encoding.UTF8;
-
-                result = _webClient.DownloadString(url);
-                result = Regex.Match(result, REGEX).Groups[1].Value.Trim();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
-            finally
-            {
-                Cursor.Current = Cursors.Default;
-            }
-
-            return result;
-        }
         private void BtnTran_Click(object sender, EventArgs e)
         {
             this.Visible = false;
             try
             {
                 // Initialize the translator
-                Translator translator = new Translator();
-
+                if (_translator == null)
+                {
+                    _translator = new Translator();
+                }
+                
                 this.Cursor = Cursors.WaitCursor;
 
                 _inputLang = DevTool.Properties.Settings.Default.InputLang;
@@ -160,12 +127,11 @@ namespace DevTool.Translation
                 _copyToClipboard = DevTool.Properties.Settings.Default.CopyClipBoard;
 
                 string inputText = Clipboard.GetText().Trim();
-                string baseUrl = translator.TranslationhUrl;
-                translator.Translate(inputText, _inputLang, _outputLang);
+                _translator.Translate(inputText, _inputLang, _outputLang);
 
-                if (translator.Error == null)
+                if (_translator.Error == null)
                 {
-                    if (string.IsNullOrEmpty(translator.TranslationResultText))
+                    if (string.IsNullOrEmpty(_translator.TranslationResultText))
                     {
                         if (MessageBox.Show(
                                 Resources.ERR001,
@@ -174,7 +140,7 @@ namespace DevTool.Translation
                                 MessageBoxIcon.Error
                             ) == DialogResult.Yes)
                         {
-                            System.Diagnostics.Process.Start(translator.TranslationhUrl);
+                            System.Diagnostics.Process.Start(_translator.TranslationhUrl);
                         }
                     }
                     else
@@ -182,18 +148,18 @@ namespace DevTool.Translation
                         // Copy if setting = true
                         if (_copyToClipboard)
                         {
-                            Clipboard.SetText(translator.TranslationResultText);
+                            Clipboard.SetText(_translator.TranslationResultText);
                         }
 
                         if (_frmResult == null || _frmResult.IsDisposed)
                         {
                             _frmResult = new TranslationResult();
-                            _frmResult.InitControll(translator);
+                            _frmResult.InitControll(_translator);
                             _frmResult.Show();
                         }
                         else
                         {
-                            _frmResult.InitControll(translator);
+                            _frmResult.InitControll(_translator);
                             // Set the form to top window
                             _frmResult.Focus();
                         }
@@ -202,7 +168,7 @@ namespace DevTool.Translation
                 }
                 else
                 {
-                    MessageBox.Show(translator.Error.Message, Resources.TitleError, MessageBoxButtons.OK,
+                    MessageBox.Show(_translator.Error.Message, Resources.TitleError, MessageBoxButtons.OK,
                         MessageBoxIcon.Exclamation);
                 }
             }
